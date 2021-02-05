3f93a6a0d74310f520da4011f4df726ea13245db
Cyru8 <olumide.adetiba@live.com>
2021-02-04 19:41:40 -0800

    Reorganized directories per providers.

--- aws/terraform-sensitive-variables/variables.tf
+++ aws/terraform-sensitive-variables/variables.tf
@@ -54,13 +54,13 @@ variable "instance_type" {
 
 variable "db_username" {
   description = "Database administrator username"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "db_password" {
   description = "Database administrator password"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
