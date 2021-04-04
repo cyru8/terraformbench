7f8d49c35fd13a69e1dd4bd80ee784c5123f84ee
Cyru8 <olumide.adetiba@live.com>
2021-04-03 19:43:15 -0700

    Added prod manifest

---
+++ aws/staggingprodservices/prod/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">=0.12.26"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "webserver_cluster" {
+  source = "../../modules/webserver_cluster"
+
+  cluster_name = "webservers-prod"
+  instance_type = "t2.micro"
+  min_size = 2
+  max_size = 10
+}
