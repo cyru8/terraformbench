8bfe20686f757bb85b23b37fc0757286e222ba09
Cyru8 <olumide.adetiba@live.com>
2021-06-28 20:36:46 -0700

    change after validation and fmt

--- aws/spinup-ec2-instances/main.tf
+++ aws/spinup-ec2-instances/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "techpro-ec2-instances" {
-  count = 3
-  ami = var.ami
+  count         = 3
+  ami           = var.ami
   instance_type = var.instance_type
   tags = {
     "Name" = "techpro-${count.index}"
--- aws/spinup-ec2-instances/output.tf
+++ aws/spinup-ec2-instances/output.tf
@@ -1,3 +1,3 @@
 output "ec2_machines" {
-  value = aws_instance.techpro-ec2-instances.*.arn  # This indicates that there are more than one arn (amazon resource name) based on the count declaration.
+  value = aws_instance.techpro-ec2-instances.*.arn # This indicates that there are more than one arn (amazon resource name) based on the count declaration.
 }
