b6b1ec137cf52f61de3d3857a49a05ff8d56c833
Cyru8 <olumide.adetiba@live.com>
2021-07-12 07:15:17 -0700

    Initial commit

---
+++ aws/cka-study-notes-and-examples/podswithdockersockmount.sh
@@ -0,0 +1,5 @@
+kubectl get pods --all-namespaces \
+-o=jsonpath='{range .items[*]}{"\n"}{.metadata.namespace}{":\t"}{.metadata.name}{":\t"}{range .spec.volumes[*]}{.hostPath.path}{", "}{end}{end}' \
+| sort \
+| grep '/var/run/docker.sock'
+
