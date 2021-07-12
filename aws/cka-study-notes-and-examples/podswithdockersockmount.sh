919a843f7059207720126e39263182b66b8f87ef
Cyru8 <olumide.adetiba@live.com>
2021-07-12 07:19:12 -0700

    moved cka out of aws directory

--- aws/cka-study-notes-and-examples/podswithdockersockmount.sh
+++
@@ -1,5 +0,0 @@
-kubectl get pods --all-namespaces \
--o=jsonpath='{range .items[*]}{"\n"}{.metadata.namespace}{":\t"}{.metadata.name}{":\t"}{range .spec.volumes[*]}{.hostPath.path}{", "}{end}{end}' \
-| sort \
-| grep '/var/run/docker.sock'
-
